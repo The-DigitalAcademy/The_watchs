5887e63e0a47c02b23a8e6693b5f432f744bf1a3
unknown <sabu0550@gmail.com>
2023-05-16 09:37:52 +0200

    add everything

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,29 +3,21 @@ import Container from 'react-bootstrap/Container';
 import { Link } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="./">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="./">HOME</Link>
-            <Link href="./service">ABOUT</Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">SERVICE</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">BLOG</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="./">HOME</Nav.Link>
+            <Nav.Link href="./about">ABOUT</Nav.Link>
+            <Nav.Link href="./service">SERVICE</Nav.Link>
+            <Nav.Link href="./blog">BLOG</Nav.Link>
+            <Nav.Link href="./contact">CONTACT</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
